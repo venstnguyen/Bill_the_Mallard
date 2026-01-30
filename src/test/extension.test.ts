8fc97be424b10f5e81d3ca36276d8fe2849ce367
venstnguyen@gmail.com <139813710+venstnguyen@users.noreply.github.com>
2026-01-29 23:12:11 -0700

    Adding Testing! SN

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -4,7 +4,7 @@ import * as assert from 'assert';
 // as well as import your extension to test it
 import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
-
+//Testing
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
